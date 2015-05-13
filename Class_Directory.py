880bd52c58546525e6298458bfaac86da0041dc8
CyphFalcon <CyphFalcon@users.noreply.github.com>
2015-05-13 16:14:01 -0400

    Update Class_Directory.py
    Added Player and item templates

--- Class_Directory.py
+++ Class_Directory.py
@@ -1,40 +1,60 @@
 class CardTemplate:
-	name = "Card_Name"
-	power = 0
-	tech = 0
-	will = 0
-	speed = 0
-	level = 1
-	experience = 0
-	rarity = 1
-	# Below should be auto-generated or given a default before being grown
-	roll = 0 #dribble
-	loss = 0 #steal
-	react_spd = 0 #action speed
-	observation = 0 #reflexes
-	max_HP = 0 #duh
-	support = 0 #pass
-	defense = 0 #duh
-	recovery = 0 #duh
-	critical = 0 #duh
-	crit_dam = 1.5 #duh
-	loss_resist = 0 #counter_resist
-	crit_resist = 0 #crit_resist
+	def __init__(self):
+		self.name = "Card_Name"
+		self.power = 0
+		self.tech = 0
+		self.will = 0
+		self.speed = 0
+		self.level = 1
+		self.experience = 0
+		self.rarity = 1
+		# Below should be auto-generated or given a default before being grown
+		self.roll = 0 #dribble
+		self.loss = 0 #steal
+		self.react_spd = 0 #action speed
+		self.observation = 0 #reflexes
+		self.max_HP = 0 #duh
+		self.support = 0 #pass
+		self.defense = 0 #duh
+		self.recovery = 0 #duh
+		self.critical = 0 #duh
+		self.crit_dam = 1.5 #duh
+		self.loss_resist = 0 #counter_resist
+		self.crit_resist = 0 #crit_resist
 
-	HP = 0 # HP actual
-	action = 0 #action bar actual (max 100)
+		self.HP = 0 # HP actual
+		self.action = 0 #action bar actual (max 100)
 
-	#Skills go here
-	points = 0
-	active = 1 #skills out of five
-	active_text = ""
-	active_desc = ""
-	passive1 = 0
-	passive1_text = ""
-	passive1_desc = ""
-	passive2 = 0
-	passive2_text = ""
-	passive2_desc = ""
-	passive3_text = ""
-	passive3 = 0
-	passive3_desc = ""
+		#Skills go here
+		self.points = 0
+		self.active = 1 #skills out of five
+		self.active_text = ""
+		self.active_desc = ""
+		self.passive1 = 0
+		self.passive1_text = ""
+		self.passive1_desc = ""
+		self.passive2 = 0
+		self.passive2_text = ""
+		self.passive2_desc = ""
+		self.passive3_text = ""
+		self.passive3 = 0
+		self.passive3_desc = ""
+
+class ItemTemplate:
+	def __init__(self):
+		self.item_name = ""
+		self.item_alignment = ""
+		self.item_rarity = 1
+		self.item_desc = ""
+
+class Player:
+	def __init__(self):
+		self.userID = ""
+		self.rank = 0
+		self.grade = ""
+		self.avatar = CardTemplate()
+		self.funds = ""
+		self.alignment = ""
+
+		self.roster = [CardTemplate() for i in range(100)]
+		self.inventory = [ItemTemplate() for i in range(100)]
