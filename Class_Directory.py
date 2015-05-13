42041928fb2d4800c49703879349580b96b4f59f
CyphFalcon <CyphFalcon@users.noreply.github.com>
2015-05-13 12:39:42 -0400

    Class File
    Contains all public use classes

---
+++ Class_Directory.py
@@ -0,0 +1,40 @@
+class CardTemplate:
+	name = "Card_Name"
+	power = 0
+	tech = 0
+	will = 0
+	speed = 0
+	level = 1
+	experience = 0
+	rarity = 1
+	# Below should be auto-generated or given a default before being grown
+	roll = 0 #dribble
+	loss = 0 #steal
+	react_spd = 0 #action speed
+	observation = 0 #reflexes
+	max_HP = 0 #duh
+	support = 0 #pass
+	defense = 0 #duh
+	recovery = 0 #duh
+	critical = 0 #duh
+	crit_dam = 1.5 #duh
+	loss_resist = 0 #counter_resist
+	crit_resist = 0 #crit_resist
+
+	HP = 0 # HP actual
+	action = 0 #action bar actual (max 100)
+
+	#Skills go here
+	points = 0
+	active = 1 #skills out of five
+	active_text = ""
+	active_desc = ""
+	passive1 = 0
+	passive1_text = ""
+	passive1_desc = ""
+	passive2 = 0
+	passive2_text = ""
+	passive2_desc = ""
+	passive3_text = ""
+	passive3 = 0
+	passive3_desc = ""
