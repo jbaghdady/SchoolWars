acf27a80577be1fc2ecdf4f718fde1a61a4147be
jbaghdady <jbaghda@g.clemson.edu>
2015-06-09 09:13:05 -0400

    Update player_demo.py

--- player_demo.py
+++ player_demo.py
@@ -89,19 +89,70 @@ def init_card(x):
 	x.action = 0 #action bar actual (max 100)
 
 	#Skills go here
-	x.active = 1 #skills out of five
-	x.active_text = "The future is ours!"
-	x.active_desc = "Increase strike by 7.5% (x" + str(x.active) + ")"
-	x.passive1 = 0
-	x.passive1_text = "Unbound"
-	x.passive1_desc = "Increase Critical Resistance and Loss Resistance by 10% (x" + str(x.passive1) +")"
-	x.passive2 = 0
-	x.passive2_text = "Complete Notes"
-	x.passive2_desc = "Increase Attack and Defense by 5 (x" + str(x.passive2) + ")"
-	x.passive3_text = "Prepared"
-	x.passive3 = 0
-	x.passive3_desc = "Reduce damage taken when attacked by 5% (x" + str(x.passive3) + ")"
+	x.active1 = 1 #skills out of five
+	x.active1_text = "Huh. So that's a thing that can happen."
+	x.active1_desc = "Increase Reflexes and Steal by 20% (x" + str(x.active1) + ")"
+	x.passive1_1 = 0
+	x.passive1_1_text = "Blue Hoodie"
+	x.passive1_1_desc = "Increase Defense, Recovery, and max HP by 3% (x" + str(x.passive1_1) + ")"
+	x.passive1_2 = 0
+	x.passive1_2_text = "Oversight"
+	x.passive1_2_desc = "Increase team will and power by 1 and decrease HP by 1 (x" + str(x.passive1_2) + ")"
+	x.passive1_3_text = "Preparations"
+	x.passive1_3 = 0
+	x.passive1_3_desc = "Reduce damage taken when attacked by 15% (x" + str(x.passive1_3) + ")"
+	
+	x.active2 = 2 # Baghs code
+	x.active2_text = "Here we go again!"
+	x.active2_desc = "Boost Stamina by 10% (x" + str(x.active2) + ")"
+	x.passive2_1 = 0
+	x.passive2_1_text = "Green Hoodie...Wait. What?"
+	x.passive2_1_desc = "Boost max HP by 12% (x" + str(x.passive2_1) + ")"
+	x.passive2_2 = 0
+	x.passive2_2_text = "What is wrong with this picture?"
+	x.passive2_2_desc = "Boost Stamina by 3% and decrease max HP by 10% (x" + str(x.passive2_2) + ")"
+	x.passive2_3 = 0
+	x.passive2_3_text = "Hands"
+	x.passive2_3_desc = "Boost team will by 4% and decrease max HP by 5% (x" + str(x.passive2_3) + ")"
+	
+	x.active3 = 3
+	x.active3_text = "Mine Mine Mine!!!"
+	x.active3_desc = "Boost Reflexes and Steal by 25% (x" + str(x.active3) + ")"
+	x.passive3_1 = 0
+	x.passive3_1_text = "Purple Hoodie."
+	x.passive3_1_desc = "Boost Recovery by 5% (x" + str(x.passive3_1) + ")"
+	x.passive3_2 = 0
+	x.passive3_2_text = "What is right with this picture?"
+	x.passive3_2_desc = "Boost HP by 2% and decrease Power by 4% (x" + str(x.passive3_2) + ")"
+	x.passive3_3 = 0
+	x.passive3_3_text = "CALEBBBBBB!!!!!!! MAKE IT STOPPPPP!!!"
+	x.passive3_3_desc = "Boost team will by 2% and increase attack damage 20% (x" + str(x.passive3_3) + ")"
+	
+	x.active4 = 4
+	x.active4_text = "Are we having fun yet?"
+	x.active4_desc = "Boost Recovery, Power and max HP by 10% (x" + str(x.active4) + ")"
+	x.passive4_1 = 0
+	x.passive4_1_text = "Polka-dot Hoodie (You knew it was coming)"
+	x.passive4_1_desc = "Boost Recovery by 25% (What else did you expect from polka-dot?) (x" + str(x.passive4_1) + ")"
+	x.passive4_2 = 0
+	x.passive4_2_text = "I'm beginning to think this isn't really soccer."
+	x.passive4_2_desc = "Decrease attack damage by 14% (x" + str(x.passive4_2) + ")"
+	x.passive4_3 = 0
+	x.passive4_3_text = "And suddenly: Kapadia"
+	x.passive4_3_desc = "Decrease Recovery, Power, Stamina, max HP, Reflexes, Defense, Steal by 30% (x" + str(x.passive4_3) + ")"
 
+	x.active5 = 5
+	x.active5_text = "A Wild Pikachu Appears!"
+	x.active5_desc = "Boost Stamina, max HP and Reflexes by 20% (x" + str(x.active5) + ")"
+	x.passive5_1 = 0
+	x.passive5_1_text = "RAINBOW HOODIE"
+	x.passive5_1_desc = "Decrease attack damage by 50% (x" + str(x.passive5_1) + ")"
+	x.passive5_2 = 0
+	x.passive5_2_text = "The Neymar"
+	x.passive5_2_desc = "Increase max HP, Recovery, Stamina and Reflexes by 40% (x" + str(x.passive5_2) + ")"
+	x.passive5_3 = 0
+	x.passive5_3_text = "The Bill Reid (aka the Reverse Kapadia)"
+	x.passive5_3_desc = "Increase Recovery, Power, Stamina, max HP, Reflexes, Defense Steal by 30% (x" + str(x.passive5_3) + ")"
 
 def gen_player(x,usename):
 	x.userID = usename
